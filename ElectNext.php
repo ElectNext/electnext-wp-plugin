2081dcfc5a87db82813ac42a14391eff7f44b8c6
Michael Toppa <public@toppa.com>
2012-12-13 10:16:33 -0500

    fixed saving post meta (needed to check if post was a revision); list all saved pols

--- ElectNext.php
+++ ElectNext.php
@@ -24,10 +24,6 @@ class ElectNext {
   }
 
   public function run() {
-    // put all your initializing hooks and filters here, for example...
-    //add_action('admin_menu', array($this, 'init_settings_menu'));
-    //add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
-    //add_shortcode('shashin', array($this, 'handle_shortcode'));
     add_action('add_meta_boxes', array($this, 'init_meta_box'));
     add_action('save_post', array($this, 'save_meta_box_data'));
     return true;
@@ -37,23 +33,34 @@ class ElectNext {
     add_meta_box('electnext', 'ElectNext', array($this, 'render_meta_box'), 'post', 'normal', 'high');
   }
 
-  public function render_meta_box() {
-    global $post;
+  public function render_meta_box($post) {
     $meta_pols = get_post_meta($post->ID, 'electnext_pols', true);
-    $pols = isset($meta_pols) ? unserialize($meta_pols) : array();
-    $pol_name = !empty($pols) ? $pols[0]['name'] : '';
-    $pol_id = !empty($pols) ? $pols[0]['id'] : '';
+    $pols = empty($meta_pols) ? array() : $meta_pols;
     wp_nonce_field('electnext_meta_box_nonce', 'electnext_meta_box_nonce');
     ?>
+      <div style="float: left;">
       <p>Temporary inputs to test saving</p>
       <p>
         <label for="electnext_pol_name">Politician Name</label>
-        <input type="text" name="electnext_pol_name" id="electnext_pol_name" value="<?php echo $pol_name; ?>" />
+        <input type="text" name="electnext_pol_name" id="electnext_pol_name" />
       </p>
       <p>
-        <label for="electnext_pol_id">Politician Name</label>
-        <input type="text" name="electnext_pol_id" id="electnext_pol_id" value="<?php echo $pol_id; ?>" />
+        <label for="electnext_pol_id">Politician ID</label>
+        <input type="text" name="electnext_pol_id" id="electnext_pol_id" />
       </p>
+      </div>
+      <div style="float: left; margin-left: 30px;">
+        <?php
+          if (!empty($pols)) {
+            echo '<ul>';
+            foreach ($pols as $pol) {
+              echo "<li>ID: {$pol['id']} Name: {$pol['name']}</li>";
+            }
+            echo '</ul>';
+          }
+        ?>
+      </div>
+      <div class="clear"></div>
     <?php
   }
 
@@ -62,10 +69,14 @@ class ElectNext {
     if (!isset( $_POST['electnext_meta_box_nonce']) || !wp_verify_nonce($_POST['electnext_meta_box_nonce'], 'electnext_meta_box_nonce')) return;
     if (!current_user_can('edit_post')) return;
 
-    $meta_pols = get_post_meta($post_id, 'electnext_pols', true);
-    $pols = isset($meta_pols) ? unserialize($meta_pols) : array();
+    if ($parent_id = wp_is_post_revision($post_id)) {
+      $post_id = $parent_id;
+    }
 
+    $meta_pols = get_post_meta($post_id, 'electnext_pols', true);
+    $pols = empty($meta_pols) ? array() : $meta_pols;
     $existing = false;
+
     if (isset($_POST['electnext_pol_name']) && isset($_POST['electnext_pol_id'])) {
       for ($i=0; $i < count($pols); ++$i) {
         if ($pols[$i]['id'] == $_POST['electnext_pol_id']) {
@@ -80,7 +91,7 @@ class ElectNext {
       $pols[] = array('name' => esc_attr($_POST['electnext_pol_name']), 'id' => esc_attr($_POST['electnext_pol_id']));
     }
 
-    update_post_meta($post_id, 'electnext_pols', serialize($pols));
+    update_post_meta($post_id, 'electnext_pols', $pols);
   }
 
   public function render_exception_message($e) {
