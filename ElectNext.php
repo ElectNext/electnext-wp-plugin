f438eb20642fbed0a60e873f7ffea059aaf70843
Michael Toppa <public@toppa.com>
2012-12-17 14:17:39 -0500

    added search by name

--- ElectNext.php
+++ ElectNext.php
@@ -2,6 +2,7 @@
 
 class ElectNext {
   private $version = '0.1';
+  private $site_url = 'https://electnext.dev';
   private $utils;
 
   public function __construct($utils) {
@@ -38,51 +39,78 @@ class ElectNext {
     $meta_pols = get_post_meta($post->ID, 'electnext_pols', true);
     $pols = empty($meta_pols) ? array() : $meta_pols;
     wp_nonce_field('electnext_meta_box_nonce', 'electnext_meta_box_nonce');
+    echo "<script async src='{$this->site_url}/api/v1/info_widget.js'></script>\n";
     ?>
 
-    <script async src="https://electnext.dev/api/v1/info_widget.js"></script>
 
     <script>
       jQuery(document).ready(function($) {
-        // for later
-        //$('#electnext_add_pol').click(function(ev) {
-        //  $('<p><label for="electnext_pol[][id]">ID:</label> <input type="text" name="electnext_pol[][id]"> <label for="electnext_pol[][name]">Name:</label> <input type="text" name="electnext_pol[][name]"></p>').appendTo(electnext_pols);
-        //  ev.preventDefault();
-        //});
+        function electnext_add_to_list(pol) {
+          $('#electnext-pols ul').append(
+            '<li class="electnext-pol" id="electnext-pol-id-' + pol.id + '">'
+            + '<strong>' + pol.name + '</strong>'
+            + (pol.title ? (' - <span>' + pol.title + '</span>') : '')
+            + '<i style="display:none;">' + pol.id + '</i>'
+            + ' [ <a href="#" class="electnext-pol-remove">x</a> ]</li>'
+          );
+        }
+        // this relies on jquery-ui-sortable being loaded
+        $('#electnext-pols ul').sortable();
 
-        // scan the post content for politician names, and add ones we find to the meta box
+        // scan the post content for politician names, and add ones we find to the list
         $('#electnext-scan-btn').on('click', function(ev) {
           ev.preventDefault();
           var content = tinyMCE.get('content').getContent().replace(/(<([^>]+)>)/ig,"");
           var possibles = ElectNext.scan_string(content);
           ElectNext.search_candidates(possibles, function(data) {
-            $.each(data, function(idx, el) {
-              $('#electnext-pols p em').empty();
-              if (!$('#electnext-pol-id-' + el.id).length) {
-                $('#electnext-pols ul').append(
-                  '<li class="electnext-pol" id="electnext-pol-id-' + el.id + '">'
-                    + '<strong>' + el.name + '</strong>'
-                    + (el.title ? (' - <span>' + el.title + '</span>') : '')
-                    + '<i style="display:none;">' + el.id + '</i>'
-                    + ' [ <a href="#" class="electnext-pol-remove">x</a> ]</li>'
-                );
-              }
-            })
-
             if (data.length == 0) {
-              $('#electnext-pols p em').text('No names found');
+              $('#electnext-scan em').text('No names found');
+            }
+
+            else {
+              $('#electnext-scan em').empty();
+              $.each(data, function(idx, el) {
+                if (!$('#electnext-pol-id-' + el.id).length) {
+                  electnext_add_to_list(el);
+                }
+              })
             }
           });
         });
 
-
-        // th
-        $('#electnext-pols ul').sortable();
-
-        // remove requested names
+        // remove names on demand
         $('#electnext-pols ul').on('click', '.electnext-pol-remove', function(ev) {
           ev.preventDefault();
           $(this).parent().remove();
+        });
+
+        // search by name and add the results to the list
+        $('#electnext-find-btn').on('click', function(ev) {
+          ev.preventDefault();
+
+          if ($('#electnext-search-name').val().length == 0) {
+            alert('Please enter the name of a politician.');
+          }
+
+          else {
+            var electnext_search_url = "<?php echo $this->site_url; ?>/api/v1/name_search.js?callback=?";
+            var dataToSend = { q: $('#electnext-search-name').val() };
+
+            $.getJSON(electnext_search_url, dataToSend, function(dataReceived) {
+              if (dataReceived.length == 0) {
+                $('#electnext-search em').text('No results found');
+              }
+
+              else {
+                $('#electnext-search em').empty();
+                $.each(dataReceived, function(idx, el) {
+                  if (!$('#electnext-pol-id-' + el.id).length) {
+                    electnext_add_to_list(el);
+                  }
+                });
+              }
+            });
+          }
         });
 
         // save the final set of names when the post is saved
@@ -95,7 +123,7 @@ class ElectNext {
               + '<input type="hidden"'
                 + ' name="electnext_pols_meta[' + i + '][name]"'
                 + ' value="' + $('.electnext-pol:eq(' + i + ') strong').text() + '">'
-                + '<input type="hidden"'
+              + '<input type="hidden"'
                 + ' name="electnext_pols_meta[' + i + '][title]"'
                 + ' value="' + $('.electnext-pol:eq(' + i + ') span').text() + '">'
             );
@@ -104,14 +132,15 @@ class ElectNext {
       });
 
     </script>
+    <div style="float: right; margin-left: 10px;">
+      <p id="electnext-search">
+        <label for="electnext-search-name">Add a politician by name:</label>
+        <br><input type="text" name="electnext-search-name" id="electnext-search-name">
+        <br><a class="button" href="#" id="electnext-find-btn">Find</a> <em></em>
+      </p>
+    </div>
+
     <div id="electnext-pols">
-        <?php
-
-        /* for later
-       <p><a href="#" id="electnext_add_pol">Add another</a></p>
-        */
-        ?>
-
       <ul>
       <?php
         if (!empty($pols)) {
@@ -120,15 +149,16 @@ class ElectNext {
               . "<strong>{$pols[$i]['name']}</strong>"
               . (strlen($pols[$i]['title']) ? " - <span>{$pols[$i]['title']}</span>" : "")
               . "<i style='display:none;'>{$pols[$i]['id']}</i>"
-              . " [ <a href='#' class='electnext-pol-remove'>x</a> ]</li>";
+              . " [ <a href='#' class='electnext-pol-remove'>x</a> ]</li>\n";
           }
         }
       ?>
       </ul>
 
-      <p><a class="button" href="#" id="electnext-scan-btn">Scan post</a> <em></em></p>
-      <div class="clear"></div>
+      <p id="electnext-scan"><a class="button" href="#" id="electnext-scan-btn">Scan post</a> <em></em></p>
     </div>
+    <div class="clear"></div>
+
     <?php
   }
 
@@ -141,7 +171,7 @@ class ElectNext {
     update_post_meta($post_id, 'electnext_pols', $pols);
   }
 
-  function add_jquery_ui_sortable($hook) {
+  public function add_jquery_ui_sortable($hook) {
     // jquery-ui-sortable is automatically included in the post editor in WP 3.5
     // but we don't want to assume it always will be in future versions, so enqueue it
     if ($hook == 'post-new.php' || $hook == 'post.php') {
